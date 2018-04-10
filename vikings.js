5ad624ec299d26bea8450eaedab538656de61007
ElyCruzGuzman <elycruzguzman7@hotmail.com>
2018-04-10 10:36:46 +0200

    create viking

---
+++ vikings.js
@@ -0,0 +1,41 @@
+var Viking = function(name, health, strength){
+	this.name = name;
+	this.health = health;
+	this.strength = strength;
+}
+Viking.prototype.atack = function(){
+	console.log(this.strength)
+}
+
+
+
+var rolo = new Viking("rolo", 100, 80);
+var laguerza = new Viking("laguerza", 90, 70);
+
+function pit(viking1, viking2){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var Saxon = function(health, strength){
+	this.health = health;
+	this.strength = strength;
+}
+
